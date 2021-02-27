5db5cbc5ce8f4cc9fdbb53e467b6ebff0404ded1
pan_fera <pan_fera@mail.ru>
2021-02-27 22:18:17 +0300

    lambda

--- include/MultithreadsSHA.hpp
+++ include/MultithreadsSHA.hpp
@@ -32,20 +32,6 @@
 #include <nlohmann/json.hpp>
 #include <mutex>
 
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/attributes/attribute.hpp>
-#include <boost/log/attributes/attribute_cast.hpp>
-#include <boost/log/attributes/attribute_value.hpp>
-
 #ifndef TEMPLATE_MULTITHREADSSHA_HPP
 #define TEMPLATE_MULTITHREADSSHA_HPP
 
--- sources/MultithreadsSHA.cpp
+++ sources/MultithreadsSHA.cpp
@@ -12,143 +12,127 @@ namespace expr = boost::log::expressions;
 namespace keywords = boost::log::keywords;
 
 //std::atomic<bool> state = true;
-std::mutex mutex;
 
-std::vector<nlohmann::json> values;
+std::mutex mutex;
+static auto result = nlohmann::json::array();
+
 std::string _filename;
 
 MultithreadsSHA::MultithreadsSHA():_count(std::thread::hardware_concurrency()){
-  _filename = "";
+    _filename = "";
 }
 
 MultithreadsSHA::MultithreadsSHA(unsigned int count){
-  _count = (count <= std::thread::hardware_concurrency())? count :
-           std::thread::hardware_concurrency();
-  _filename = "";
+    _count = (count <= std::thread::hardware_concurrency())? count :
+             std::thread::hardware_concurrency();
+    _filename = "";
 }
 
 MultithreadsSHA::MultithreadsSHA(unsigned int count, std::string filename){
-  _count = (count <= std::thread::hardware_concurrency())? count :
-           std::thread::hardware_concurrency();
-  _filename = filename;
+    _count = (count <= std::thread::hardware_concurrency())? count :
+             std::thread::hardware_concurrency();
+    _filename = filename;
 }
 
 [[noreturn]] void MultithreadsSHA::find_hash() {
-  std::string hash;
-  int number;
-  std::string hex_number;
-  nlohmann::json _j;
+    std::string hash;
+    int number;
+    std::string hex_number;
+    nlohmann::json j;
 
-  std::srand(static_cast<unsigned int>(std::time(nullptr)));
-  src::severity_logger<logging::trivial::severity_level> slg;
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-  while(true) {
-    int milli_seconds = 0;
-    auto start = std::chrono::high_resolution_clock::now();
+    while(true) {
+        int milli_seconds = 0;
+        auto start = std::chrono::high_resolution_clock::now();
+        do {
+            number = std::rand();
+            std::stringstream stream;
 
-    do {
-      number = std::rand();
-      std::stringstream stream;
+            stream << std::hex << number;
+            hex_number = stream.str();
 
-      stream << std::hex << number;
-      hex_number = stream.str();
+            const std::vector<unsigned char> data_vector(hex_number.begin(),
+                                                         hex_number.end());
 
-      const std::vector<unsigned char> data_vector(hex_number.begin(),
-                                                   hex_number.end());
+            hash = picosha2::hash256_hex_string(data_vector);
 
-      hash = picosha2::hash256_hex_string(data_vector);
-      //if (state) mutex.lock();
-      BOOST_LOG_TRIVIAL(trace) << " data: " << hex_number
-                               << " hash: " << hash << " id: " <<  std::this_thread::get_id();
-    } while (hash.substr(60) != "0000");
-    auto end = std::chrono::high_resolution_clock::now();
-    milli_seconds +=
-        std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
-            .count();
-    BOOST_LOG_TRIVIAL(info)<< " data: " << hex_number
-                           << " hash: " << hash << " id: "
-                           <<  std::this_thread::get_id();
-    nlohmann::json j = {
-        {"timestamp (ms)", milli_seconds}, {"hash", hash}, {"data", hex_number}};
-    values.push_back(j);
-  }
+            BOOST_LOG_TRIVIAL(trace) << " data: " << hex_number
+                                     << " hash: " << hash << " id: " <<  std::this_thread::get_id();
+        } while (hash.substr(60) != "0000");
+        auto end = std::chrono::high_resolution_clock::now();
+        milli_seconds +=
+                std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                        .count();
+        BOOST_LOG_TRIVIAL(info)<< " data: " << hex_number
+                               << " hash: " << hash << " id: "
+                               <<  std::this_thread::get_id();
+        auto make_object = [&]
+        {
+            auto js = nlohmann::json::object();
+            js["timestamp(ms)"] = milli_seconds;
+            js["hash"] = hash;
+            js["data"] = hex_number;
+            return js;
+        };
+        result.push_back(make_object());
+    }
 
 }
 
 void MultithreadsSHA::start() {
-  std::vector<std::thread> th;
-  signal(SIGTERM, ex);
-  signal(SIGKILL, ex);
-  signal(SIGABRT, ex);
-  signal(SIGINT, ex);
-  init();
+    std::vector<std::thread> th;
+    signal(SIGTERM, ex);
+    signal(SIGKILL, ex);
+    signal(SIGABRT, ex);
+    signal(SIGINT, ex);
 
-  for (unsigned int i = 0; i < _count; ++i) {
-    std::thread _thread(find_hash);
-    th.push_back(std::move(_thread));
-  }
+    init();
 
-  for (std::thread& _th : th) {
-    _th.join();
-  }
+    for (unsigned int i = 0; i < _count; ++i) {
+        std::thread _thread(find_hash);
+        th.push_back(std::move(_thread));
+    }
+
+    for (std::thread& _th : th) {
+        _th.join();
+    }
 
 }
 
 void MultithreadsSHA::init() {
-  logging::add_console_log(std::cout,
-                           keywords::format = "[%TimeStamp%] [%Severity%] %Message%",
-                           keywords::auto_flush = true,
-                           keywords::filter = logging::trivial::severity == logging::trivial::info);
+    logging::add_console_log(std::cout,
+                             keywords::format = "[%TimeStamp%] [%Severity%] %Message%",
+                             keywords::auto_flush = true,
+                             keywords::filter = logging::trivial::severity == logging::trivial::info);
 
-  typedef sinks::synchronous_sink<sinks::text_file_backend> file_sink;
-  boost::shared_ptr<file_sink> sink(new file_sink(
-      keywords::file_name = "./logs/file_%5N.log",
-      keywords::rotation_size = 5 * 1024 * 1024,
-      keywords::time_based_rotation =
-          sinks::file::rotation_at_time_point(12, 0, 0),
-      keywords::auto_flush = true));
+    typedef sinks::synchronous_sink<sinks::text_file_backend> file_sink;
+    boost::shared_ptr<file_sink> sink(new file_sink(
+            keywords::file_name = "./logs/file_%5N.log",
+            keywords::rotation_size = 5 * 1024 * 1024,
+            keywords::time_based_rotation =
+                    sinks::file::rotation_at_time_point(12, 0, 0),
+            keywords::auto_flush = true));
 
-  sink->set_formatter(expr::stream
-                          << "[" << expr::attr<boost::posix_time::ptime>("TimeStamp")
-                          << "] [" << logging::trivial::severity << "] "
-                          << expr::smessage);
+    sink->set_formatter(expr::stream
+                                << "[" << expr::attr<boost::posix_time::ptime>("TimeStamp")
+                                << "] [" << logging::trivial::severity << "] "
+                                << expr::smessage);
 
-  logging::core::get()->add_sink(sink);
-  logging::add_common_attributes();
-  /*
-  const std::string format = "[%TimeStamp%] [%Severity%] %Message%";
-
-  auto sink_to_file = logging::add_file_log(
-      boost::log::keywords::file_name = "logs/log_trace_%N.log",
-      boost::log::keywords::rotation_size = 10*1024*1024,
-      boost::log::keywords::format = format);
-  sink_to_file->set_filter(
-      boost::log::trivial::severity >= boost::log::trivial::trace);
-
-  auto sink_to_console = boost::log::add_console_log(
-      std::cout,
-      boost::log::keywords::format = format);
-  sink_to_console->set_filter(
-      boost::log::trivial::severity >= boost::log::trivial::info);
-
-  boost::log::add_common_attributes();*/
+    logging::core::get()->add_sink(sink);
+    logging::add_common_attributes();
 }
 
 void MultithreadsSHA::ex([[maybe_unused]] int sig_num) {
-   mutex.lock();
- // state = false;
-  //logging::core::get()->flush();
-  // logging::core::get()->remove_all_sinks();
+    mutex.lock();
 
-  write_json();
-  exit(sig_num);
+    write_json();
+    exit(sig_num);
 }
 
 void MultithreadsSHA::write_json() {
-  std::ofstream file;
-  file.open(_filename + ".json");
-  for (unsigned i = 0; i < values.size(); ++i){
-    file << values[i].dump(5) << "\n";
-  }
-  file.close();
+    std::ofstream file;
+    file.open(_filename + ".json");
+    file << result.dump(4);
+    file.close();
 }
